c4b753545d0ac400f0cd03051432d88f3d63d6cf
Sami Rollins <srollins@cs.usfca.edu>
2015-10-08 15:40:52 -0700

    adding logging example

--- code/Servlets/src/HelloServlet.java
+++ code/Servlets/src/HelloServlet.java
@@ -1,27 +1,34 @@
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.logging.Level;
 
-import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.sun.istack.internal.logging.Logger;
+
 
 public class HelloServlet extends HttpServlet {
 
+	
+	private Logger logger = Logger.getLogger(HelloServlet.class);
+	
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
 			
-        response.setContentType("text/html");
-        response.setStatus(HttpServletResponse.SC_OK);
-		
-		PrintWriter out = response.getWriter();
-
 		String name = request.getParameter("name");
 		if(name == null) {
 			name = "anonymous";
 		}		
+
+		logger.log(Level.INFO, "Request from: " + name);
 		
-		out.println("<html><title>HelloServlet</title><body>Hello, " + name + "!<br/>Thanks for visiting</body></html>");
+		response.setContentType("text/html");
+        response.setStatus(HttpServletResponse.SC_OK);
+                
+		PrintWriter out = response.getWriter();
+		
+		out.println("<html><title>HelloServlxet</title><body>Hello, " + name + "!<br/>Thanks for visiting</body></html>");
 		
 	}
 	
